42e7fa98328545f3688808bcfad3cc18cc7c3d7e
namratapanda1 <68112151+namratapanda1@users.noreply.github.com>
2020-07-16 17:59:39 +0530

    Update application.py

--- application.py
+++ application.py
@@ -62,4 +62,4 @@ if __name__ == '__main__':
     model = load_model()  # load model at the beginning once only
     print('Model loaded')
     print(model)
-    #app.run(port=5000)
+    app.run(host='0.0.0.0',port=5000)
